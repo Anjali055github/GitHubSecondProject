fc69e7292e24d394d003bc388c87bcdc6cad37b0
Anjali Jadhav <anjali.javadeveloper55@gmail.com>
2023-07-13 19:57:42 +0530

    New ArrayList package is added

---
+++ src/com/velocity/arraylist/ArraylistDemo.java
@@ -0,0 +1,22 @@
+package com.velocity.arraylist;
+
+public class ArraylistDemo {
+
+	public static void main(String[] args) {
+		
+			int[] arr=new int[10];
+			System.out.println("Git phase1");
+			for(int i=0;i<10;i++) {
+				arr[i]=i+1;
+			}
+			
+			
+			for(int x:arr)
+				System.out.print("Numbers"+x);
+			System.out.println("hello");
+
+		}
+
+	
+
+}
